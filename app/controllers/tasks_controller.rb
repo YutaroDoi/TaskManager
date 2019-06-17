ff309add5840a777edd2c46c3d15579c11ed54fa
土井 優太朗 <1111842@Macbook-doi.local>
2019-06-17 17:57:58 +0900

    タスク作成、編集画面作成&スタイルの修正、パーシャルへの分割

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,4 +7,37 @@ class TasksController < ApplicationController
     def show
       @task = Task.find(params[:id])
     end
+
+    def new
+      @task = Task.new
+    end
+
+    def create
+      @task = Task.new(task_params)
+      if @task.save
+        redirect_to root_path
+        flash[:success] = "タスクを作成しました！"
+      else
+        render 'new'
+      end
+    end
+
+    def edit
+      @task = Task.find(params[:id])
+    end
+
+    def update
+      @task = Task.find(params[:id])
+      if @task.update_attributes(task_params)
+        flash[:success] = "タスクを編集しました！"
+        redirect_to root_path
+      else
+        render 'edit'
+      end
+    end
+
+    private
+      def task_params
+        params.require(:task).permit(:name, :label, :priority, :expire, :description)
+      end
 end
