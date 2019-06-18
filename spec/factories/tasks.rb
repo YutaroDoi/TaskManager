18c34d12da2e1211245d74bc1bebd6b1d6453b1d
土井 優太朗 <1111842@Macbook-doi.local>
2019-06-18 19:22:33 +0900

    Taskモデルのvalidationテスト作成

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
     factory :task do
       name { '掃除' }
       description { '家の掃除をする' }
-      expire　{ Time.now }
+      expire { Time.now }
       status { '未実施' } 
       label { '家事' }
       priority { 1 }
---
+++ spec/models/task_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+RSpec.describe Task, type: :model do
+  it "is valid with name and status" do
+    task = create(:task)
+    expect(task).to be_valid
+  end
+end
