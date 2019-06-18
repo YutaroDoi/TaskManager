eee1bca333b87794896bff7839ac614fe0a7987a
土井 優太朗 <1111842@Macbook-doi.local>
2019-06-18 14:57:57 +0900

    RSpecの導入

---
+++ spec/factories/tasks.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do 
+    factory :task do
+      name { '掃除' }
+      description { '家の掃除をする' }
+      expire　{ Time.now }
+      status { '未実施' } 
+      label { '家事' }
+      priority { 1 }
+    end
+end
