8cd49d386fa99abe4f26e5cd309bd6657efc260a
土井 優太朗 <1111842@Macbook-doi.local>
2019-06-20 15:22:31 +0900

    nameとstatusにindex追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_19_090228) do
+ActiveRecord::Schema.define(version: 2019_06_20_061927) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 2019_06_19_090228) do
     t.integer "priority"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["name", "status"], name: "index_tasks_on_name_and_status"
   end
 
 end
