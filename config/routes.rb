2fd892b8e774d1c2dc610c4c153f3a641e9eb524
土井 優太朗 <1111842@Macbook-doi.local>
2019-06-20 19:43:06 +0900

    ログイン画面、sessions_controller追加、ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  root 'tasks#index'
+  root 'sessions#new'
+  get '/signup', to:'users#new'
+  post '/signup', to:'users#create'
+  get '/login', to:'sessions#new'
+  post '/login', to:'sessions#create'
+  delete '/logout', to:'sessions#destroy'
   resources :tasks
 end
