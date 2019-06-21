0b5766cf6183e7fb2d3a51b48d992df7bde90952
土井 優太朗 <1111842@Macbook-doi.local>
2019-06-21 15:41:58 +0900

    スキーマ変更反映

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,20 @@ class UsersController < ApplicationController
     def new
         @user = User.new
     end
+
     def create
+        @user = User.new(user_params)
+        if @user.save
+            redirect_to tasks_path
+            flash[:success] = "新規登録完了！"
+        else
+            render 'new'
+        end
     end
+
+    private
+      def user_params
+        params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      end
 
 end
