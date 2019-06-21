0580640e554b1b6e2d6d686fddcbc1678c34c60d
土井 優太朗 <1111842@Macbook-doi.local>
2019-06-21 17:29:18 +0900

    save前にemailをdowncaseするフック

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
   has_many :tasks, dependent: :destroy
+
+  before_save :downcase_email
+
   validates :name, presence: true, length: { maximum: 50 }
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -10,4 +13,10 @@ class User < ApplicationRecord
   has_secure_password
 
   validates :password, length: { minimum: 6 }, presence: true, allow_nil: true
+
+
+  private
+    def downcase_email
+      self.email.downcase!
+    end
 end
