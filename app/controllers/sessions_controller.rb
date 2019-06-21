71c7290facc3d79efce82877df3435531cc2539e
土井 優太朗 <1111842@Macbook-doi.local>
2019-06-21 19:26:46 +0900

    AuthenticationによるセッションCookieの付与

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,17 @@ class SessionsController < ApplicationController
     end
     
     def create
+        @user = User.find_by(email: params[:session][:email].downcase)
+        if @user && @user.authenticate(params[:session][:password])
+            log_in @user
+            redirect_to tasks_path
+        else
+            flash.now[:danger] = '不正なメールアドレスとパスワードの組み合わせです'
+            render 'new'
+        end
     end
 
     def destroy
     end
+
 end
