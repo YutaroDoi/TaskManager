f26c3459341131e5b5277d4a3d3187da0043ee4e
土井 優太朗 <1111842@Macbook-doi.local>
2019-06-24 12:18:19 +0900

    ユーザー作成画面作成

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -16,8 +16,8 @@ class Admin::UsersController < ApplicationController
         def create
             @user = User.new(user_params)
             if @user.save
-                redirect_to tasks_path
-                flash[:success] = "新規登録完了！"
+                redirect_to admin_users_path
+                flash[:success] = "ユーザー作成完了！"
             else
                 render 'new'
             end
