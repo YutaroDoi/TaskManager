aff53baa20e203a5d1124095ab2cc90a4268b47a
土井 優太朗 <1111842@Macbook-doi.local>
2019-06-24 19:13:04 +0900

    TaskとLabelの中間モデル生成とmigration

---
+++ db/migrate/20190624101149_create_task_labels.rb
@@ -0,0 +1,8 @@
+class CreateTaskLabels < ActiveRecord::Migration[5.2]
+  def change
+    create_table :task_labels do |t|
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_24_100511) do
+ActiveRecord::Schema.define(version: 2019_06_24_101149) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "labels", force: :cascade do |t|
     t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "task_labels", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
