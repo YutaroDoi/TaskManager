4a50b51b391fafdc0b01746f02697e6eab6777d3
土井 優太朗 <1111842@Macbook-doi.local>
2019-06-25 13:32:41 +0900

    Modelにaccepts_nested_attrributes_for追加, 中間モデルに対し外部キー制約をdestroyに

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,9 @@
 class Task < ApplicationRecord
     belongs_to :user
+    has_many :task_labels, dependent: :destroy
     has_many :labels, through: :task_labels
-    has_many :task_labels
+    accepts_nested_attributes_for :task_labels, allow_destroy: true
+
 
     validates :name, presence: true, length: { maximum: 50 }
     validates :status, presence: true
