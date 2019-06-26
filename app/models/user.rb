9e92623def109ca59b297fac5aaa81728f8c3de7
土井 優太朗 <1111842@Macbook-doi.local>
2019-06-26 18:12:42 +0900

    パスワードのvalidationはユーザー生成時のみかける

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,8 +12,7 @@ class User < ApplicationRecord
   uniqueness: { case_sensitive: false }
   has_secure_password
 
-  validates :password, length: { minimum: 6 }, presence: true, allow_nil: true
-
+  validates :password, length: { minimum: 6 }, presence: true, on: :create
 
   private
     def downcase_email
