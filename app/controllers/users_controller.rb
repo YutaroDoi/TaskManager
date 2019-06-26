820dda8ec9eeb342717667833a588d31298e3f4f
土井 優太朗 <1111842@Macbook-doi.local>
2019-06-26 11:49:17 +0900

    新規登録成功時にログイン処理も実行

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+            log_in @user
             redirect_to tasks_path
             flash[:success] = "新規登録完了！"
         else
