458a417f552cfd7b9f687a5f79bc3543a83884a7
土井 優太朗 <1111842@Macbook-doi.local>
2019-06-26 11:00:34 +0900

    Labelのvalidation

--- app/models/label.rb
+++ app/models/label.rb
@@ -1,4 +1,6 @@
 class Label < ApplicationRecord
     has_many :task_labels, dependent: :destroy
     has_many :tasks, through: :task_labels
+
+    validates :name, presence: true, length: { maximum: 50 }
 end
